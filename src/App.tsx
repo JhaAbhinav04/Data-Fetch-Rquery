22f7edc639e387486fe853f6e195f06b8d535cb5
Abhinav Jha <abhinavrocx@gmail.com>
2023-05-05 20:29:07 +0530

    fetch data with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 // https://jsonplaceholder.typicode.com/posts/
 
-function App() {
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 
-  return (
-    <>
-      <h1>React Typescript Webpack Starter Template</h1>
-    </>
-  )
+// Typescript essentials
+interface Data {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+function App() {
+  const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+  const { data, isLoading } = useQuery({
+    queryFn: async () => {
+      // longer loading state
+      await wait(2000)
+
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts/1'
+      )
+      return data as Data
+    },
+  })
+
+  // render entire object
+  return <div>{isLoading ? 'Content is Loading':JSON.stringify(data)}</div>
 }
 
 export default App
